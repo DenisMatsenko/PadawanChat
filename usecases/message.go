0c052ba7f6b0d2d256ca24f0bc21381f997f555d
hard.tender.blade <92087058+DenisMatsenko@users.noreply.github.com>
2023-04-04 01:33:25 +0200

    Create message.go

---
+++ usecases/message.go
@@ -0,0 +1,50 @@
+package usecases
+
+import (
+	"Chat/domain"
+	"Chat/ports"
+)
+
+type MessageUsecase struct {
+	dbStorage *ports.DbStorage
+}
+
+func NewMessageUsecase(pdb *ports.DbStorage) *MessageUsecase {
+	return &MessageUsecase{dbStorage: pdb}
+}
+
+func (msgu MessageUsecase) InsertToDb(message domain.Message) error {
+	err := msgu.dbStorage.InsertToDb(message)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (msgu MessageUsecase) DeleteFromDb(messageId int) error {
+	err := msgu.dbStorage.DeleteFromDb(messageId)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (msgu MessageUsecase) GetAllFromDb() ([]domain.Message, error) {
+	rows, err := msgu.dbStorage.GetAllFromDb()
+	if err != nil {
+		return nil, err
+	}
+
+	var messages []domain.Message
+
+	for rows.Next() {
+		var message domain.Message
+		err = rows.Scan(&message.Id, &message.Content, &message.Author)
+		if err != nil {
+			return nil, err
+		}
+		messages = append(messages, message)
+	}
+
+	return messages, nil
+}
